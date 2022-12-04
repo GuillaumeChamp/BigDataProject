415e70b5500023f16134f79a8f08c6c77447e43d
guillaume <guillaume.champtoussel@ensea.fr>
2022-12-04 17:02:38 +0100

    Initial commit

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -2,7 +2,7 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.log4j.{Level, Logger}
 object MyApp {
-    def main(args : Array[String]) {
+    def main(args : Array[String]): Unit = {
         Logger.getLogger("org").setLevel(Level.WARN)
         val conf = new SparkConf().setAppName("JavaWordCount").setMaster("local[2]").set("spark.executor.memory", "1g")
         val sc = new SparkContext(conf)
@@ -10,6 +10,6 @@ object MyApp {
         val data = sc.textFile("src/main/resources/98.txt")
         val numAs = data.filter(line => line.contains("a")).count()
         val numBs = data.filter(line => line.contains("b")).count()
-        println(s"Lines with a: ${numAs}, Lines with b: ${numBs}")
+        println(s"Lines with a: $numAs, Lines with b: $numBs")
     }
 }
