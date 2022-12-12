e5f0b89fa06944c110ef216fe9d0684501b9ebd6
guillaume <guillaume.champtoussel@ensea.fr>
2022-12-12 19:51:57 +0100

    end of filtering and casting

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -1,6 +1,6 @@
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.col
 import org.apache.spark.{SparkConf, SparkContext}
 
 
@@ -10,7 +10,7 @@ object MyApp {
         Logger.getLogger("org").setLevel(Level.WARN)
 
         val conf = new SparkConf().setAppName("FlightDelayLearning").setMaster("local[2]").set("spark.executor.memory", "1g")
-        val sc = new SparkContext(conf)
+        new SparkContext(conf)
 
         val spark = SparkSession
             .builder()
@@ -20,14 +20,26 @@ object MyApp {
 
         val dataframe = spark.read.options(Map("inferSchema"->"true","delimiter"->",","header"->"true"))
             .csv("src/main/resources/1998.csv")
-        dataframe.printSchema()
 
         //val dropped = ("ArrTime","ActualElapsedTime","AirTime","TaxiIn","TaxiOut","Cancelled","CancellationCode","Diverted","CarrierDelay","WeatherDelay","NASDelay","SecurityDelay","LateAircraftDelay")
 
         val filtered = dataframe.drop(
-            "ArrTime","ActualElapsedTime","AirTime","TaxiIn","TaxiOut","Cancelled","CancellationCode","Diverted","CarrierDelay","WeatherDelay","NASDelay","SecurityDelay","LateAircraftDelay")
-        filtered.printSchema()
+            "ArrTime", "ActualElapsedTime", "AirTime", "TaxiIn", "TaxiOut", "Cancelled", "CancellationCode", "Diverted", "CarrierDelay", "WeatherDelay", "NASDelay", "SecurityDelay", "LateAircraftDelay")
+        val pattern = "^[0-9]"
+        val filtered_regex = filtered.filter(_.getString(4).matches(pattern)).filter(_.getString(10).matches(pattern)).filter(_.getString(11).matches(pattern)).filter(_.getString(12).matches(pattern))
+            .select(filtered.columns.map {
+            case column@"DepTime" =>
+                col(column).cast("int").as(column)
+            case column@"CRSElapsedTime" =>
+                col(column).cast("int").as(column)
+            case column@"ArrDelay" =>
+                col(column).cast("int").as(column)
+            case column@"DepDelay" =>
+                col(column).cast("int").as(column)
+            case column =>
+                col(column)
+        }: _*)
 
+        filtered_regex.printSchema()
     }
-
 }
